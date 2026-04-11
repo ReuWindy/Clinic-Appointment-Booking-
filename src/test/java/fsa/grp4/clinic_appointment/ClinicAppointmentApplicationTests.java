d965e2cd99f8fd431e24214e119676ca4028a7e4
tuda <daoanhtu2k2@gmail.com>
2026-04-11 20:44:21 +0700

    init

---
+++ src/test/java/fsa/grp4/clinic_appointment/ClinicAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package fsa.grp4.clinic_appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
