24d359fcedadb85bd7b9a036956f2dfdd5a17ed4
DHuy2003 <duchuy07062003@gmail.com>
2026-05-06 09:04:11 +0700

    update api specialty

--- src/main/java/fsa/grp4/clinic_appointment/config/SecurityConfig.java
+++ src/main/java/fsa/grp4/clinic_appointment/config/SecurityConfig.java
@@ -35,8 +35,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/api/auth/**",
-                                "/api/admin/**"
+                                "/api/auth/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 );
