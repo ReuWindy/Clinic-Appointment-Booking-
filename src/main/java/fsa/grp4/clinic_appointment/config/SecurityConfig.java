ce900cfcab9236d06101ad7892a8b7a24b37ac65
tuda <daoanhtu2k2@gmail.com>
2026-05-19 09:16:31 +0700

    fix: pwa config

--- src/main/java/fsa/grp4/clinic_appointment/config/SecurityConfig.java
+++ src/main/java/fsa/grp4/clinic_appointment/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
                 configuration.setAllowedOrigins(List.of(
                                 "http://localhost:5173",
                                 "http://127.0.0.1:5173",
-                                "http://localhost:1734",
+                                "http://localhost:4713",
                                 "http://127.0.0.1:4173"));
                 configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
                 configuration.setAllowedHeaders(List.of("*"));
